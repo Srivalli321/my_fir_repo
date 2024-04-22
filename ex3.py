1d1ab8d90f3abbfeecc2cc4f2f56c61797c0da00
Srivalli <yamparala.sri@gmail.com>
2024-04-22 23:36:07 +0200

    this is second python file add in to the newproject repositary

---
+++ ex3.py
@@ -0,0 +1,11 @@
+if True:
+    print('true')
+    print('some times we will get error')
+n = int(input('enter nember='))
+for n in range(100):
+ if n == n/2:
+    print('print prime numbers',n)
+ elif n != n/2:
+    print('print odd numbers',n )
+
+
