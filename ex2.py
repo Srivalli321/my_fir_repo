11b9e9c8f3efbf2599fb8a39c0b6744c703fa399
Srivalli <yamparala.sri@gmail.com>
2024-04-22 23:14:21 +0200

    This is first add python file

---
+++ ex2.py
@@ -0,0 +1,13 @@
+print(''''""''')
+a=3
+print(f'print a value={a}')
+print=('a=%d',a)
+#print('a',int(a))
+#print(f"printavaluea={a}")
+v_1 = "Twinkle,"  
+v_2= "twinkle,"
+v_3 = " little star,"
+print(v_1,  v_2 , v_3)
+print("\t How i wonder what you are! ")
+a,b,c= 1,2,3
+print(a,b,c)
